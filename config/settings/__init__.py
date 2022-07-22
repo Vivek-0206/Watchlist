a85251313cfa247a5d002132fb1211bdf335b632
VIVEK <Vivek.17beceg105@gmail.com>
2022-07-22 23:35:15 +0530

    env

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,7 +1,5 @@
-from email.mime import image
-
-
 import os
+print("os.environ: ", os.environ)
 if 'ENVIRONMENT' in os.environ and os.environ['ENVIRONMENT'] == 'production':
     from .production_settings import *
 else:
